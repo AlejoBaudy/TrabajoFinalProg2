31a31fc8e7d1eb3e21a6be0d83257f54a6b9b605
FranMartini <fmartini@udesa.edu.ar>
2025-04-01 08:57:35 -0300

    agregue la lista de productos

--- tpFinal/db/usuarios.js
+++ tpFinal/db/usuarios.js
@@ -8,7 +8,47 @@ const Usuarios= {
             DNI: 47346229,
             Foto: "/images/users/default-image.png"
         },
-     productos: [],
+     productos: [{
+        Imagen: "/images/products/img-tv-samsung-smart.jpg",
+        Producto: "Television Samsung Smart",
+        Descripcion: "70 pulgadas"
+    },{
+        Imagen: "/images/products/img-samsung-galaxy-s10.jpg",
+        Producto: "Telefono Samsung Galaxy S10",
+        Descripcion: "Telefono de ultima generacion"
+     },{
+        Imagen: "/images/products/img-macbook-pro-2019.jpg",
+        Producto: "Macbook Pro",
+        Descripcion: "Año 2019, usada, bateria en muy buen estado"
+     },{
+        Imagen: "/images/products/img-cafetera-moulinex.jpg",
+        Producto: "Cafetera Moulinex",
+        Descripcion: "Estado impecable, año 2020"
+     },{
+        Imagen: "/images/products/pelota-basket.png",
+        Producto: "Pelota de basket",
+        Descripcion: "Marca Wilson, de los Chicago Bulls"
+     },{
+        Imagen: "/images/products/lillard.jpg",
+        Producto: "Camiseta Damian Lillard",
+        Descripcion: "Oficial, año 2021"
+     },{
+        Imagen: "/images/products/iphone15.jpg",
+        Producto: "iPhone 15",
+        Descripcion: "284 GB"
+     },{
+        Imagen: "/images/products/jabulani.jpg",
+        Producto: "Adidas Jabulani",
+        Descripcion: "Pelota de futbol mundial 2010"
+     },{
+        Imagen: "/images/products/dare.jpg",
+        Producto: "Funko pop Daredevil",
+        Descripcion: "Producto de Marvel"
+     },{
+        Imagen: "/images/products/cmc.jpg",
+        Producto: "Remera Christian McCaffrey",
+        Descripcion: "Talle L"
+     }],
         
 
  }
