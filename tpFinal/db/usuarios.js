8f1da59c72ab14bad792f6499e06305a876df21e
mi usuario de github <alejobaudy@gmail.com>
2025-04-02 23:00:12 -0300

    Hecho el punto 8 y 9, se modificó el profileController.js y se hizo render para profile el mail, nombre y foto

--- tpFinal/db/usuarios.js
+++ tpFinal/db/usuarios.js
@@ -3,7 +3,7 @@ const Usuarios= {
         {
             Email: "Wschuchner@udesa.edu.ar",
             Usuario: "Wilfredo",
-            Contrasena: Wildo1506,
+            Contrasena: "Wildo1506",
             Fecha: "15/06/2006",
             DNI: 47346229,
             Foto: "/images/users/default-image.png"
