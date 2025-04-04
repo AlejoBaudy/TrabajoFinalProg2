37bd7525737b1b459dbdfac31e2663e98ac84226
FranMartini <fmartini@udesa.edu.ar>
2025-04-04 12:19:09 -0300

    Hice puntos 10 y 11 del punto 1, mostre datos estaticos de product y de search results, falta crear mas routes para mostrar search results

--- tpFinal/controllers/productController.js
+++ tpFinal/controllers/productController.js
@@ -2,13 +2,46 @@ const Usuarios = require("../db/usuarios")
 
 const productController ={
     product: function(req,res){
-      res.render('product')
+      const productoEstatico = {
+        Imagen: "/images/products/lillard.jpg",
+        Producto: "Camiseta Damian Lillard",
+        Descripcion: "Oficial, año 2021",
+        Comentarios: [
+         {
+            usuario: "Francisco Martini",
+            texto: "Jugadorazo y excelente remera!",
+            imagenPerfil: "/images/users/Foto1.jpg"
+         }
+        ]}
+      res.render('product', {producto: productoEstatico})
     },
     productAdd: function(req,res){
       res.render('product-add')
     },
     searchResults: function(req,res){
-      res.render('search-results')
+      const productos1 = [
+        {
+          id: 1,
+          Imagen: "/images/products/img-tv-samsung-smart.jpg",
+          Producto: "Television Samsung Smart",
+          Descripcion: "70 pulgadas",
+          
+      },{
+          Imagen: "/images/products/img-samsung-galaxy-s10.jpg",
+          id: 2,
+          Producto: "Telefono Samsung Galaxy S10",
+          Descripcion: "Telefono de ultima generacion",
+          
+       },{
+          Imagen: "/images/products/img-macbook-pro-2019.jpg",
+          id: 3,
+          Producto: "Macbook Pro",
+          Descripcion: "Año 2019, usada, bateria en muy buen estado",
+          
+       }
+      ];
+
+      res.render('search-results', {productos1: productos1})
     }
   }
   module.exports = productController
