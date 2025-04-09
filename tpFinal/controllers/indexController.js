80b88ed2dc60e0cd76246b5d2bd69bef88248785
Wilfreschu <Wschuchner@udesa.edu.ar>
2025-04-09 10:25:56 -0300

    correcion de index

--- tpFinal/controllers/indexController.js
+++ tpFinal/controllers/indexController.js
@@ -1,7 +1,12 @@
-const Usuarios = require("../db/usuarios")
-const indexController ={
-    index: function(req,res){
-      res.render('header', {productos: Usuarios.productos});
+const Usuarios = require("../db/usuarios");
+
+const indexController = {
+    index: function(req, res) {
+        res.render('index', {
+            productos: Usuarios.productos,
+            nombreUsuario: Usuarios.usuario.Usuario
+        });
     }
-  }
-  module.exports = indexController
+};
+
+module.exports = indexController;
