5598220b2de94e3cb5919db255f67003236a429c
mi usuario de github <alejobaudy@gmail.com>
2025-05-30 17:36:25 -0300

    arreglando un tema de git

--- tpFinal/controllers/indexController.js
+++ tpFinal/controllers/indexController.js
@@ -1,17 +1,11 @@
 const Usuarios = require("../db/usuarios");
-const db = require("../database/models");
-const op = db.sequelize.Op;
 
 const indexController = {
     index: function(req, res) {
-
-        db.Producto.findAll()
-        .then(function(producto){
-             res.render('index', {productos: producto });
-        })
-       .catch(function(error){
-        console.log(error);
-        })
+        res.render('index', {
+            productos: Usuarios.productos,
+            nombreUsuario: Usuarios.usuario.Usuario,
+        });
     }
 };
 
