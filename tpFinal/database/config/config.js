a16d28d1f417fd0ec2ca400349e7e73db13ec763
mi usuario de github <alejobaudy@gmail.com>
2025-06-02 16:31:56 -0300

    cambio de nombre en config

--- tpFinal/database/config/config.js
+++ tpFinal/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "root",
-    "database": "TiendaNBA",
+    "database": "TrabajoFinalNBA",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
